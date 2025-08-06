f1af8581379df484c6249514f5ef5ec6f5bae6f7
Zhuoli Lu <149017795+lliicassie@users.noreply.github.com>
2025-08-06 10:44:19 +0200

    update

--- ex8.py
+++ ex8.py
@@ -1,6 +1,6 @@
 class Person:
     def __init__(self, name):
-        pass
+        self.name = name
 
     def hello(self):
-        print(f'Hello, my name is {self}')
+        print(f'Hello, my name is {self.name}')
